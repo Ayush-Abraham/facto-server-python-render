9cbdff88150aa01751cbdc8b8b349f5aa7eb9e67
Ayush Abraham <ayushabraham01@gmail.com>
2023-10-05 12:19:04 +1100

    test get

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 import requests
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/facto/<month>/<date>', methods=['GET'])
 def find_facts(date,month):
