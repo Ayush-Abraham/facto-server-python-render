64e69cc02d0b1bb97b3f2a37479eb64b894c0115
Ayush Abraham <ayushabraham01@gmail.com>
2023-10-05 11:32:29 +1100

    test get

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route('/getter', methods=['GET'])
 def find_facts():
-    request_data = request.get_json()
-    date = request_data['date']
-    month = request_data['month']
+    # request_data = request.get_json()
+    # date = request_data['date']
+    # month = request_data['month']
 
     # response = requests.get('https://today.zenquotes.io/api/'+month+'/'+date)
 
