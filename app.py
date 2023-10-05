5dfe9ce43193559baecf81d6a4893b7b7f8937b3
Ayush Abraham <ayushabraham01@gmail.com>
2023-10-05 11:08:48 +1100

    modify to get json from app, send to apizen, then return result

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def find_facts():
+    request_data = request.get_json()
+    date = request_data['date']
+    month = request_data['month']
+
+    response = requests.get('https://today.zenquotes.io/api/'+month+'/'+date)
+
+    return response
